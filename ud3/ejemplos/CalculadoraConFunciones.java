b731a82beedaa234a4fe60259c631e3fb8c98079
aurigom <uribebe05@gmail.com>
2025-11-10 17:11:19 +0100

    Add files via upload

---
+++ ud3/ejemplos/CalculadoraConFunciones.java
@@ -0,0 +1,63 @@
+package ud3.ejemplos;
+
+import java.util.Scanner;
+
+//Excepción personalizada
+class DivPorCeroException extends Exception {
+public DivPorCeroException(String mensaje) {
+   super(mensaje); 
+}
+}
+
+//Excepción personalizada
+class RestaException extends Exception {
+public RestaException(String mensaje) {
+super(mensaje); 
+}
+}
+
+class DigitoException extends Exception {
+public DigitoException(String mensaje) {
+super(mensaje); 
+}
+}
+
+
+public class CalculadoraConFunciones {
+ public static void main(String[] args)  {
+     Scanner sc = new Scanner(System.in);
+
+     System.out.print("Introduce el primer número: ");
+     double num1 = sc.nextDouble();
+
+     System.out.print("Introduce el segundo número: ");
+     double num2 = sc.nextDouble();
+
+     System.out.print("Elige operación (+, -, *, /): ");
+     Character operacion = sc.next().charAt(0); //Cambiado de char a Character para poder hacer el Ejercicio
+     
+     //Capturar la nueva excepción
+     
+     try {
+    	Utilidades.validaOperacion(operacion);
+         double resultado = Utilidades.calcular(num1, num2, operacion);
+         System.out.println("Resultado: " + resultado);
+         }     
+     catch (DigitoException e) {
+    		 System.out.println("Error: " + e.getMessage());
+    	
+     } catch (DivPorCeroException e) {
+         System.out.println("Error: " + e.getMessage());
+     } 
+     catch (RestaException e) {
+         System.out.println("Error: " + e.getMessage());
+     }
+     catch (Exception e) {
+         System.out.println("Operación no válida.");
+     }
+
+     sc.close();
+ }
+
+}
+
---
+++ ud3/ejemplos/Utilidades.java
@@ -0,0 +1,51 @@
+package ud3.ejemplos;
+
+import java.util.Scanner;
+
+
+public class Utilidades {
+	
+static void validaOperacion(Character op) throws DigitoException {
+	
+	if(Character.isLetterOrDigit(op) == true ) {
+		
+		 throw new DigitoException("Op no válida");
+	 }
+	 }
+			
+	
+ static double suma(double a, double b) {
+	 return a+b;
+ }
+ 
+ static double resta(double a, double b) throws RestaException {
+	 if(b>a)
+		 throw new RestaException("Resta negativa");
+	 return a-b;
+ }
+ 
+ static double producto(double a, double b) {
+	 return a*b;
+ }
+	
+ static double divide(double a, double b) throws DivPorCeroException {
+	 if (b == 0) {
+         throw new DivPorCeroException("No se puede dividir entre cero.");
+     }
+	 	 return a/b;
+ }
+ 
+  
+ static double calcular(double a, double b, char op) throws DivPorCeroException,RestaException {
+     switch (op) {
+         case '+': return suma(a,b);
+         case '-': return resta(a,b);
+         case '*': return producto(a,b);
+         case '/':return divide(a,b);
+         default:
+             throw new IllegalArgumentException("Operación desconocida.");
+     }
+ }
+ 
+}
+
