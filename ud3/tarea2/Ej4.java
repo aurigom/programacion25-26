b0674effabe5e6dcfbb76629a960d1d5b4aab9ea
aurigom <uribebe05@gmail.com>
2025-11-11 10:40:38 +0100

    Update Ej4.java

--- ud3/tarea2/Ej4.java
+++ ud3/tarea2/Ej4.java
@@ -1,10 +1,25 @@
 package ud3.tarea2;
 
+import java.util.Scanner;
+
 public class Ej4 {
 
 	public static void main(String[] args) {
-		//Diseñar un programa que pida una frase al usuario, e indique si el carácter de la posición central es o no un espacio
+		//  Diseñar una actividad que pida al usuario que 
+		//introduzca una frase por teclado e indique cuántos espacios en blanco tiene.
+		
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Introduce una frase: ");
+		String frase = sc.nextLine();
+		
+		int contador = 0;
+		
+		for (int i = 0; i < frase.length(); i++) {
+			if (frase.charAt(i) == ' ') {
+				contador++;
+			}
+		}
+		
+		System.out.println("La frase tiene " + contador + " espacios en blanco.");
 
-	}
-
-}
+}}
