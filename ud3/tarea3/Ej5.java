53d6f25f68afb7bbb8d1003570eba386aee5b2e0
aurigom <uribebe05@gmail.com>
2025-11-18 23:59:19 +0100

    Update Ej5.java

--- ud3/tarea3/Ej5.java
+++ ud3/tarea3/Ej5.java
@@ -14,20 +14,59 @@ public class Ej5 {
 		//resultantes continúen ordenados sin necesidad de volver a realizar una ordenación.
 		
 		Scanner sc = new Scanner (System.in);
-		int [] tabla1 = new int [5];
-		
+		int [] tabla1 = new int [6];
+		int [] tabla2 = new int [6];
 			
-			for (int i = 0; i < tabla1.length; i++) {
-				System.out.println("introduce un número: ");
-				int num=sc.nextInt();
-				tabla1[i]=sc.nextInt();
-			}
+		//recorremos el bucle 6 veces pidiendo cada vez un num y guardandolo en i
+		  System.out.println("introduce 6 números para rellenar la primera tabla: ");
+	        for (int i = 0; i < tabla1.length; i++) {
+	            System.out.print("Tabla1:");
+	            tabla1[i] = sc.nextInt();
+	        }
 			
+		//igual con el segundo
+	        System.out.println("introduce 6 números para rellenar la segunda tabla: ");
+	        for (int i = 0; i < tabla2.length; i++) {
+	            System.out.print("Tabla2: ");
+	            tabla2[i] = sc.nextInt();
+	        }
+			
+	    //odernamos las dos tablas y las imprimimos
+			Arrays.sort(tabla1);
+	        Arrays.sort(tabla2);
 		
-	
+		System.out.println("tabla 1 en orden: "+Arrays.toString(tabla1));
+		System.out.println("tabla 2 en orden: "+Arrays.toString(tabla2));
 		
-		System.out.println(Arrays.toString(tabla1));
+		
+		 int[] fusion = new int[12]; //tabla para almacenar las otras dos
+		 int i = 0; //para movernos por la primera tabla
+		 int j= 0; //para movernos por la segunda tabla
+		 int k = 0; //para movernos por la tabla fusión
+		 
+		 
+		 //mientras la i sea menor que la tabla1 y la j menor que la 2 
+		 //compara la posición de la tabla1 con la de la tabla2 y si es menor la almacena en la posición correspondente de fusión
+		 //incrementamos la i 
+		 while (i < tabla1.length && j < tabla2.length) {
+	            if (tabla1[i] <= tabla2[j]) {
+	                fusion[k] = tabla1[i];
+	                i++;
+	                
+	      // si no es menor almacenamos la posición de la tabla 2 en fusión e incrementamos la j
+	            } else {
+	                fusion[k] = tabla2[j];
+	                j++;
+	            }
+	       
+	            k++;
+	        }
+		
+		 
+		 
+		 System.out.println("fusión: " + Arrays.toString(fusion));
 
+		 
 	}
 
 }
