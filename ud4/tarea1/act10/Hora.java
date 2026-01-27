acef3a3eb02969164f6c97d8113780e327d8a5dc
aurigom <uribebe05@gmail.com>
2026-01-27 11:57:42 +0100

    Refactor Hora class to include seconds and improve logic

--- ud4/tarea1/act10/Hora.java
+++ ud4/tarea1/act10/Hora.java
@@ -1,79 +1,64 @@
 package ud4.tarea1.act10;
 
-import java.util.Scanner;
-
-
 public class Hora {
-	
-	
+
 	private int hora;
-	private int n;
-	private int minuto;
+	private int minuto; 
+	private int segundo;
 	
-	
-	
-	public Hora( ) {
+	public Hora(int hora, int minuto, int segundo) {
 		this.hora = hora;
-		this.n = n;
 		this.minuto = minuto;
+		this.segundo = segundo;
 	}
-
-
 
 	public int getHora() {
 		return hora;
 	}
 
-
-
 	public void setHora(int hora) {
 		this.hora = hora;
 	}
-
-
-
-	public int getN() {
-		return n;
-	}
-
-
-
-	public void setN(int n) {
-		this.n = n;
-	}
-
-
 
 	public int getMinuto() {
 		return minuto;
 	}
 
-
-
 	public void setMinuto(int minuto) {
 		this.minuto = minuto;
 	}
 
-
-
-	public void suma(int hora, int minuto, int n) {
-		
-		if (n>60) {
-			this.minuto+=n;
-		}
-		
-		if (minuto>60) {
-			this.hora+=minuto;
-		}
-		
+	public int getSegundo() {
+		return segundo;
 	}
 
-
+	public void setSegundo(int segundo) {
+		this.segundo = segundo;
+	}
+	
+	public void suma(int n) {
+		
+		segundo= segundo+n;
+		
+		while(segundo>=60) {
+			segundo= segundo - 60;
+			minuto++;
+		}
+		
+		while(minuto>= 60) {
+			minuto= minuto-60;
+			hora++;
+		}
+		
+		
+	}
 
 	@Override
 	public String toString() {
-		return "Hora [hora=" + hora +", minuto=" + minuto +  ", n=" + n + "]";
+		return "Hora [hora=" + hora + ", minuto=" + minuto + ", segundo=" + segundo + "]";
 	}
+	
+	
 	
 	
 }
