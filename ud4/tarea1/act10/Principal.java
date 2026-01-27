db5dc0b580db761b4638fcf311a7403e2715cad8
aurigom <uribebe05@gmail.com>
2026-01-27 11:58:03 +0100

    Update Principal.java

--- ud4/tarea1/act10/Principal.java
+++ ud4/tarea1/act10/Principal.java
@@ -3,14 +3,27 @@ package ud4.tarea1.act10;
 import java.util.Scanner;
 
 public class Principal {
+public static Scanner sc = new Scanner (System.in);
 
 	public static void main(String[] args) {
-	
-		 Hora hora = new Hora();
-		 
-		 hora.suma(12, 67, 90);
-		 System.out.println(hora.toString());
+		
+		System.out.println("Introduzca la hora:");
+		int hora = sc.nextInt();
+		
+		System.out.println("Introduzca los minutos:");
+		int minutos = sc.nextInt();
+		
+		System.out.println("Introduzca los segundos");
+		int segundos = sc.nextInt();
+		
+		Hora hora1 = new Hora(hora, minutos, segundos);
 
+		System.out.println("Introduzca la cantidad de segundos a sumar: ");
+		int n = sc.nextInt();
+		
+		hora1.suma(n);
+		
+		System.out.println(hora1.toString());
 	}
 
 }
